0c9013e213cb808306740095246b84c8c3f18300
mack <mack.hatcher1@outlook.com>
2023-11-28 16:40:25 -0600

    VaultFactory tests are now at 100% coverage.

--- test/factory.test.js
+++ test/factory.test.js
@@ -28,11 +28,11 @@ describe("Factory", function () {
     proxy = await deployContract("ManagerProxy", manager.address);
 
     vault = await deployContract(
-      "CrunaFlexiVault",
-      erc6551Registry.address,
-      guardian.address,
-      signatureValidator.address,
-      proxy.address,
+        "CrunaFlexiVault",
+        erc6551Registry.address,
+        guardian.address,
+        signatureValidator.address,
+        proxy.address,
     );
     factory = await deployContractUpgradeable("VaultFactory", [vault.address]);
 
@@ -41,7 +41,7 @@ describe("Factory", function () {
     usdc = await deployContract("USDCoin");
     usdt = await deployContract("TetherUSD");
 
-    await usdc.mint(bob.address, normalize("900"));
+    await usdc.mint(bob.address, normalize("3000"));
     await usdt.mint(alice.address, normalize("600", 6));
 
     await expect(factory.setPrice(990)).to.emit(factory, "PriceSet").withArgs(990);
@@ -58,17 +58,17 @@ describe("Factory", function () {
     const salt = ethers.utils.hexZeroPad(ethers.BigNumber.from("400").toHexString(), 32);
 
     await expect(factory.connect(bob).buyVaults(usdc.address, 1, ""))
-      .to.emit(vault, "Transfer")
-      .withArgs(addr0, bob.address, nextTokenId)
-      .to.emit(erc6551Registry, "ERC6551AccountCreated")
-      .withArgs(
-        precalculatedAddress,
-        toChecksumAddress(proxy.address),
-        salt,
-        (await getChainId()).toString(),
-        toChecksumAddress(vault.address),
-        nextTokenId,
-      );
+        .to.emit(vault, "Transfer")
+        .withArgs(addr0, bob.address, nextTokenId)
+        .to.emit(erc6551Registry, "ERC6551AccountCreated")
+        .withArgs(
+            precalculatedAddress,
+            toChecksumAddress(proxy.address),
+            salt,
+            (await getChainId()).toString(),
+            toChecksumAddress(vault.address),
+            nextTokenId,
+        );
   });
 
   async function buyVault(token, amount, buyer, promoCode = "") {
@@ -76,12 +76,12 @@ describe("Factory", function () {
     await token.connect(buyer).approve(factory.address, price.mul(amount));
 
     await expect(factory.connect(buyer).buyVaults(token.address, amount, promoCode))
-      .to.emit(vault, "Transfer")
-      .withArgs(addr0, buyer.address, 1)
-      .to.emit(vault, "Transfer")
-      .withArgs(addr0, buyer.address, 2)
-      .to.emit(token, "Transfer")
-      .withArgs(buyer.address, factory.address, price.mul(amount));
+        .to.emit(vault, "Transfer")
+        .withArgs(addr0, buyer.address, 1)
+        .to.emit(vault, "Transfer")
+        .withArgs(addr0, buyer.address, 2)
+        .to.emit(token, "Transfer")
+        .withArgs(buyer.address, factory.address, price.mul(amount));
   }
 
   it("should allow bob and alice to purchase some vaults", async function () {
@@ -94,11 +94,11 @@ describe("Factory", function () {
     expect(price.toString()).to.equal("9900000");
 
     await expect(factory.withdrawProceeds(fred.address, usdc.address, normalize("10")))
-      .to.emit(usdc, "Transfer")
-      .withArgs(factory.address, fred.address, normalize("10"));
+        .to.emit(usdc, "Transfer")
+        .withArgs(factory.address, fred.address, normalize("10"));
     await expect(factory.withdrawProceeds(fred.address, usdc.address, 0))
-      .to.emit(usdc, "Transfer")
-      .withArgs(factory.address, fred.address, amount("9.8"));
+        .to.emit(usdc, "Transfer")
+        .withArgs(factory.address, fred.address, amount("9.8"));
   });
 
   it("should allow bob and alice to purchase some vaults with a promoCode", async function () {
@@ -112,5 +112,52 @@ describe("Factory", function () {
     expect(price.toString()).to.equal("9900000000000000000");
     price = await factory.finalPrice(usdt.address, promoCode);
     expect(price.toString()).to.equal("8910000");
+  });
+
+  it("should remove a stableCoin when active is false", async function () {
+    await expect(factory.setStableCoin(usdc.address, false))
+        .to.emit(factory, "StableCoinSet")
+        .withArgs(usdc.address, false);
+
+    const updatedStableCoins = await factory.getStableCoins();
+    expect(updatedStableCoins).to.not.include(usdc.address);
+  });
+
+  it("should remove the effect of a promo code when its discount is set to zero", async function () {
+    const promoCode = "TheRoundTable".toLowerCase();
+    const discount = 10;
+    const zeroDiscount = 0;
+    const stableCoin = usdc.address;
+
+    await factory.setPromoCode(promoCode, discount);
+
+    await factory.setPromoCode(promoCode, zeroDiscount);
+    let priceAfterDiscountReset = await factory.finalPrice(stableCoin, promoCode);
+
+    let regularPrice = await factory.finalPrice(stableCoin, "");
+    expect(priceAfterDiscountReset).to.equal(regularPrice);
+  });
+
+  it("should allow batch purchase of vaults", async function () {
+    const stableCoin = usdc.address;
+    const buyers = [bob.address, bob.address];
+    const amounts = [1, 1];
+
+    let pricePerVault = await factory.finalPrice(stableCoin, "");
+    let totalPayment = pricePerVault.mul(amounts.length);
+
+    await usdc.mint(bob.address, totalPayment.add(normalize("1000")));
+
+    await usdc.connect(bob).approve(factory.address, totalPayment);
+
+    await expect(factory.connect(bob).buyVaultsBatch(stableCoin, buyers, amounts, ""))
+        .to.emit(vault, "Transfer")
+        .withArgs(addr0, buyers[0], 1)
+        .to.emit(vault, "Transfer")
+        .withArgs(addr0, buyers[1], 2);
+
+    expect(await usdc.balanceOf(factory.address)).to.equal(totalPayment);
+
+    expect(await vault.balanceOf(bob.address)).to.equal(amounts.length);
   });
 });
