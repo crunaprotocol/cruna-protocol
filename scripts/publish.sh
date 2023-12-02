63684dac8da9c22c45a8549667c5f3deb71cb66a
Francesco Sullo <sullof@gmail.com>
2023-12-02 12:07:35 -0800

    Fix contracts/package.json

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,16 +2,18 @@
 
 SCRIPT_DIR=$(dirname "$0")
 VERSION=$($SCRIPT_DIR/get-package-version.js)
-echo $VERSION
 
 cp README.md contracts/README.md
 cd contracts
 
 if [[ $VERSION == *"-alpha"* ]]; then
+  echo "Publishing alpha version $VERSION"
   npm publish --tag alpha
 elif [[ $VERSION == *"-beta"* ]]; then
+  echo "Publishing beta version $VERSION"
   npm publish --tag beta
 else
+  echo "Publishing stable version $VERSION"
   npm publish
 fi
 
