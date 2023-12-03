d6880506fb9c6203de1f0ff74caa5dbbb71c517c
Francesco Sullo <sullof@gmail.com>
2023-12-03 10:04:21 -0800

    Fix __gap s

--- contracts/manager/ManagerBase.sol
+++ contracts/manager/ManagerBase.sol
@@ -70,4 +70,10 @@ contract ManagerBase is Context, Versioned {
   function getImplementation() external view returns (address) {
     return StorageSlot.getAddressSlot(_IMPLEMENTATION_SLOT).value;
   }
+
+  // @dev This empty reserved space is put in place to allow future versions to add new
+  // variables without shifting down storage in the inheritance chain.
+  // See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+
+  uint256[50] private __gap;
 }
