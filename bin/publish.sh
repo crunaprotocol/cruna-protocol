b882d986622ae7c0c572d68078ae3a493fb6f40c
Francesco Sullo <sullof@gmail.com>
2024-03-09 16:08:17 -0800

    Fix publish.sh

--- bin/publish.sh
+++ bin/publish.sh
@@ -4,6 +4,19 @@ root_dir=$(dirname $(realpath $(dirname "$0")))
 # if not run from the root, we cd into the root
 cd $root_dir
 
+# Check if the current branch is 'main'
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+if [ "$current_branch" != "main" ]; then
+    echo "Error: Not on the 'main' branch."
+    exit 1
+fi
+
+# Check for uncommitted changes
+if ! git diff-index --quiet HEAD --; then
+    echo "Error: There are uncommitted changes."
+    exit 1
+fi
+
 # we call the script explicitly via node because if not, if the file is
 # missing, the version will just be empty and no error is returned
 version=$(node scripts/get-package-version.js)
