bd43e6698aac8cf1610ae4205a0c60a24dbd89ed
Francesco Sullo <sullof@gmail.com>
2024-03-13 17:15:44 -0700

    Add reference to Solidity-codegen

--- scripts/docs-index.js
+++ scripts/docs-index.js
@@ -54,7 +54,7 @@ let indexContent = generateIndexContent(markdownFiles, baseDir);
 
 indexContent += `
 
-This documentation is automatically generated using [solidity-docgen](
+This documentation is automatically generated using [solidity-docgen](https://github.com/OpenZeppelin/solidity-docgen)
 (c) 2024+ Cruna
 `;
 
