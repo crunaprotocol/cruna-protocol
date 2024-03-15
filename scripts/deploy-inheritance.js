61091b15614518eedc257d8adcae154bf8d87fdc
Francesco Sullo <sullof@gmail.com>
2024-03-15 15:28:09 -0700

    Deploying the plugin

--- scripts/deploy-inheritance.js
+++ scripts/deploy-inheritance.js
@@ -2,28 +2,75 @@ require("dotenv").config();
 const hre = require("hardhat");
 const ethers = hre.ethers;
 const path = require("path");
+const fs = require("fs-extra");
 const EthDeployUtils = require("eth-deploy-utils");
+// const EthDeployUtils = require("../../../Personal/deploy-utils");
+
 let deployUtils;
+
+const { expect } = require("chai");
 
 async function main() {
   deployUtils = new EthDeployUtils(path.resolve(__dirname, ".."), console.log);
 
   const chainId = await deployUtils.currentChainId();
-
   const [deployer] = await ethers.getSigners();
 
-  if (chainId === 1337) {
-    // on localhost, we deploy the factory
-    await deployUtils.deployNickSFactory(deployer);
+  process.env.CHAIN_ID = chainId;
+  require("./set-canonical");
+  const bytecodesPath = path.resolve(__dirname, "../export/deployedBytecodes.json ");
+
+  if (!fs.existsSync(bytecodesPath)) {
+    fs.writeFileSync(bytecodesPath, JSON.stringify({}));
   }
 
-  const salt = ethers.constants.HashZero;
+  let salt = "0xccccc" + "0".repeat(59);
 
-  // deploy the plugin
-  const plugin = await deployUtils.deployContractViaNickSFactory(deployer, "InheritanceCrunaPlugin", salt);
+  const bytecodes = JSON.parse(fs.readFileSync(bytecodesPath));
 
-  // deploy the plugin's proxy
-  await deployUtils.deployContractViaNickSFactory(deployer, "InheritanceCrunaPluginProxy", ["address"], [plugin.address], salt);
+  if (!bytecodes.InheritanceCrunaPlugin) {
+    bytecodes.InheritanceCrunaPlugin = {
+      salt,
+    };
+    bytecodes.InheritanceCrunaPluginProxy = {
+      salt,
+    };
+  }
+
+  if (!bytecodes.InheritanceCrunaPlugin.bytecode || process.env.OVERRIDE) {
+    bytecodes.InheritanceCrunaPlugin.bytecode = await deployUtils.getBytecodeToBeDeployedViaNickSFactory(
+      deployer,
+      "InheritanceCrunaPlugin",
+    );
+  }
+
+  let plugin = await deployUtils.deployBytecodeViaNickSFactory(
+    deployer,
+    "InheritanceCrunaPlugin",
+    bytecodes.InheritanceCrunaPlugin.bytecode,
+    salt,
+  );
+
+  bytecodes.InheritanceCrunaPlugin.address = plugin.address;
+
+  if (!bytecodes.InheritanceCrunaPluginProxy.bytecode || process.env.OVERRIDE) {
+    bytecodes.InheritanceCrunaPluginProxy.bytecode = await deployUtils.getBytecodeToBeDeployedViaNickSFactory(
+      deployer,
+      "InheritanceCrunaPluginProxy",
+      ["address"],
+      [plugin.address],
+    );
+  }
+
+  let proxy = await deployUtils.deployBytecodeViaNickSFactory(
+    deployer,
+    "InheritanceCrunaPluginProxy",
+    bytecodes.InheritanceCrunaPluginProxy.bytecode,
+    salt,
+  );
+  bytecodes.InheritanceCrunaPluginProxy.address = proxy.address;
+
+  fs.writeFileSync(bytecodesPath, JSON.stringify(bytecodes, null, 2));
 }
 
 main()
