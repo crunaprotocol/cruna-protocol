c94aacb330e1afd7d0dee4937dd8c0fd9a14841f
Francesco Sullo <sullof@gmail.com>
2024-04-04 13:20:36 -0700

    Fix variable shadowing

--- bin/flatten.sh
+++ bin/flatten.sh
@@ -11,5 +11,10 @@ FOLDER=""
 if [[ "$2" != "" ]]; then
   FOLDER=$2/
 fi
+
+cp libs-canonical/not-localhost/Canonical.sol contracts/libs/.
+
 NODE_ENV=test npx hardhat flatten contracts/$FOLDER$1.sol > flattened/$1-flattened.sol
 scripts/clean-licenses-in-flattened.js $1
+
+cp libs-canonical/localhost/Canonical.sol contracts/libs/.
--- contracts/manager/CrunaManager.sol
+++ contracts/manager/CrunaManager.sol
@@ -365,7 +365,7 @@ contract CrunaManager is Actor, CrunaManagerBase {
    * @notice see {IProtectedNFT-protectedTransfer}.
    */
   function protectedTransfer(
-    uint256 tokenId,
+    uint256 tokenId_,
     address to,
     uint256 timestamp,
     uint256 validFor,
@@ -374,7 +374,7 @@ contract CrunaManager is Actor, CrunaManagerBase {
     if (timestamp == 0) revert ToBeUsedOnlyWhenProtectorsAreActive();
     _preValidateAndCheckSignature(this.protectedTransfer.selector, to, 0, 0, 0, timestamp, validFor, signature);
     _resetOnTransfer(bytes4(0), bytes4(0));
-    _vault().managedTransfer(_nameId(), tokenId, to);
+    _vault().managedTransfer(_nameId(), tokenId_, to);
   }
 
   // Internal function to get an instance of the plugin
